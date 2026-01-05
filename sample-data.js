004c43ab756e837dc819a6f4cde24ca9704561b3
vituud2 <overroot0@gmail.com>
2026-01-05 13:29:45 -0300

    05-01-26 1

--- sample-data.js
+++ sample-data.js
@@ -1,147 +1,153 @@
 // Optional: you can delete this file if you don't want a sample.
 // app.js expects window.SAMPLE_DATA to exist (or it will just start empty).
 window.SAMPLE_DATA = `
-     {
-        "Nome": "Eleonilda Rodrigues Gomes",
-        "codigo_de_rastreio": [
-            "AN403363800BR"
-        ],
+         {
+        "Nome": "Kennia Thania de Oliveira",
+        "codigo_de_rastreio": [],
+        "telefone": null,
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014579867406",
+        "cidade": "Indiaporã",
+        "estado": "SP",
+        "done": false
+    },
+    {
+        "Nome": "Maria Alice Campos Mesquita da Silva",
+        "codigo_de_rastreio": [],
+        "telefone": null,
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014578755492",
+        "cidade": "São João da Boa Vista",
+        "estado": "SP",
+        "done": false
+    },
+    {
+        "Nome": "Rafael Maragno Probst",
+        "codigo_de_rastreio": [],
+        "telefone": null,
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014577230118",
+        "cidade": "Novo Hamburgo",
+        "estado": "RS",
+        "done": false
+    },
+    {
+        "Nome": "Edson Machado Wergutz",
+        "codigo_de_rastreio": [],
+        "telefone": null,
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014565264108",
+        "cidade": "Chapecó",
+        "estado": "SC",
+        "done": false
+    },
+    {
+        "Nome": "Neife Barbiere Neia",
+        "codigo_de_rastreio": [],
+        "telefone": null,
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014554380280",
+        "cidade": "Curitiba",
+        "estado": "PR",
+        "done": false
+    },
+    {
+        "Nome": "Nicoli Martins",
+        "codigo_de_rastreio": [],
+        "telefone": null,
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014553491970",
+        "cidade": "São José",
+        "estado": "SC",
+        "done": false
+    },
+    {
+        "Nome": "Tatiane Fernanda de Carvalho",
+        "codigo_de_rastreio": [],
+        "telefone": null,
+        "codigo_de_marketplace": "ML2",
+        "codigo_de_cada_marketplace": "2000014541486902",
+        "cidade": "Poços de Caldas",
+        "estado": "MG",
+        "done": false
+    },
+    {
+        "Nome": "Leandro Feres",
+        "codigo_de_rastreio": [],
         "telefone": null,
         "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014379855898",
-        "cidade": "Santo André",
+        "codigo_de_cada_marketplace": "2000014538122144",
+        "cidade": "São Paulo",
         "estado": "SP",
         "done": false
     },
     {
-        "Nome": "Jair Donizeti Balestra",
-        "codigo_de_rastreio": [
-            "AN403363795BR"
-        ],
+        "Nome": "Andrea Maranhao de Oliveira Dantas Abrantes",
+        "codigo_de_rastreio": [],
         "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014380514032",
-        "cidade": "Auriflama",
-        "estado": "SP",
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014533660100",
+        "cidade": "João Pessoa",
+        "estado": "PB",
         "done": false
     },
     {
-        "Nome": "Patricia Lutterbach Arslanian Araujo",
-        "codigo_de_rastreio": [
-            "AN403363606BR"
-        ],
+        "Nome": "Gabriel Longuinhos Queiroz",
+        "codigo_de_rastreio": [],
+        "telefone": null,
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014534280758",
+        "cidade": "Frutal",
+        "estado": "MG",
+        "done": false
+    },
+    {
+        "Nome": "Andre Balduino Berte",
+        "codigo_de_rastreio": [],
         "telefone": null,
         "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014385775192",
-        "cidade": "Rio de Janeiro",
-        "estado": "RJ",
+        "codigo_de_cada_marketplace": "2000014518258190",
+        "cidade": "Porto Alegre",
+        "estado": "RS",
         "done": false
     },
     {
-        "Nome": "Jose Paulo Celestrini",
-        "codigo_de_rastreio": [
-            "AN403363623BR"
-        ],
+        "Nome": "Nathalie Do Nascimento Pires",
+        "codigo_de_rastreio": [],
         "telefone": null,
         "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014357939408",
-        "cidade": "Olímpia",
-        "estado": "SP",
+        "codigo_de_cada_marketplace": "2000014509748100",
+        "cidade": "Osório",
+        "estado": "RS",
         "done": false
     },
     {
-        "Nome": "Paulo Cesar Sanches",
-        "codigo_de_rastreio": [
-            "AN403363610BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014390679242",
-        "cidade": "Avaré",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Ricardo Pereira Boaventura",
-        "codigo_de_rastreio": [
-            "AN403363597BR"
-        ],
+        "Nome": "Dorival Fernandes Dos Santos",
+        "codigo_de_rastreio": [],
         "telefone": null,
         "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014396498804",
-        "cidade": "Campinas",
+        "codigo_de_cada_marketplace": "2000014502818116",
+        "cidade": "São Paulo",
         "estado": "SP",
         "done": false
     },
     {
-        "Nome": "Ricardo Pereira Boaventura",
-        "codigo_de_rastreio": [
-            "AN403363583BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014396609250",
-        "cidade": "Campinas",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Ricardo Pereira Boaventura",
-        "codigo_de_rastreio": [
-            "AN403363447BR"
-        ],
+        "Nome": "Naif Abdo Faris",
+        "codigo_de_rastreio": [],
         "telefone": null,
         "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014396558584",
-        "cidade": "Campinas",
-        "estado": "SP",
+        "codigo_de_cada_marketplace": "2000014487580738",
+        "cidade": "Vilhena",
+        "estado": "RO",
         "done": false
     },
     {
-        "Nome": "Ricardo Pereira Boaventura",
-        "codigo_de_rastreio": [
-            "AN403363274BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014396501692",
-        "cidade": "Campinas",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Ricardo Pereira Boaventura",
-        "codigo_de_rastreio": [
-            "AN403363288BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014396551332",
-        "cidade": "Campinas",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Joao Marques Luiz Neto",
-        "codigo_de_rastreio": [
-            "AN403363433BR"
-        ],
+        "Nome": "Luis Cezar Abib",
+        "codigo_de_rastreio": [],
         "telefone": null,
         "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014408087216",
-        "cidade": "Guarulhos",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Paulo Omar Saba",
-        "codigo_de_rastreio": [
-            "AN403363291BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014431501910",
-        "cidade": "Piracicaba",
-        "estado": "SP",
+        "codigo_de_cada_marketplace": "2000014474275152",
+        "cidade": "Mallet",
+        "estado": "PR",
         "done": false
     }`.trim();
