5322fa6b49b5428dd9c76f3ae7ea9373884e736f
vituud2 <overroot0@gmail.com>
2026-01-07 15:09:52 -0300

    chore: data update 07-01-25

--- sample-data.js
+++ sample-data.js
@@ -2,206 +2,26 @@
 // app.js expects window.SAMPLE_DATA to exist (or it will just start empty).
 window.SAMPLE_DATA = `
          {
-        "Nome": "Kennia Thania de Oliveira",
+        "Nome": "Debora Bueno Muniz Oliveira",
         "codigo_de_rastreio": [
-            "AN441089388BR"
+            "AN451936030BR"
         ],
         "telefone": null,
         "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014579867406",
-        "cidade": "Indiaporã",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Maria Alice Campos Mesquita da Silva",
-        "codigo_de_rastreio": [
-            "AN441089391BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014578755492",
-        "cidade": "São João da Boa Vista",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Rafael Maragno Probst",
-        "codigo_de_rastreio": [
-            "AN441089365BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014577230118",
-        "cidade": "Novo Hamburgo",
-        "estado": "RS",
-        "done": false
-    },
-    {
-        "Nome": "Edson Machado Wergutz",
-        "codigo_de_rastreio": [
-            "AN441089343BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014565264108",
-        "cidade": "Chapecó",
-        "estado": "SC",
-        "done": false
-    },
-    {
-        "Nome": "Neife Barbiere Neia",
-        "codigo_de_rastreio": [
-            "AN441089374BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014554380280",
-        "cidade": "Curitiba",
-        "estado": "PR",
-        "done": false
-    },
-    {
-        "Nome": "Nicoli Martins",
-        "codigo_de_rastreio": [
-            "AN441089697BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014553491970",
-        "cidade": "São José",
-        "estado": "SC",
-        "done": false
-    },
-    {
-        "Nome": "Tatiane Fernanda de Carvalho",
-        "codigo_de_rastreio": [
-            "AN441089683BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014541486902",
-        "cidade": "Poços de Caldas",
-        "estado": "MG",
-        "done": false
-    },
-    {
-        "Nome": "Leandro Feres",
-        "codigo_de_rastreio": [
-            "AN441089670BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014538122144",
+        "codigo_de_cada_marketplace": "2000014610404936",
         "cidade": "São Paulo",
         "estado": "SP",
         "done": false
     },
     {
-        "Nome": "Andrea Maranhao de Oliveira Dantas Abrantes",
+        "Nome": "Danilo Prado Pires",
         "codigo_de_rastreio": [
-            "AN441089706BR"
+            "AN451936043BR"
         ],
         "telefone": null,
         "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014533660100",
-        "cidade": "João Pessoa",
-        "estado": "PB",
-        "done": false
-    },
-    {
-        "Nome": "Gabriel Longuinhos Queiroz",
-        "codigo_de_rastreio": [
-            "AN441089723BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014534280758",
-        "cidade": "Frutal",
-        "estado": "MG",
-        "done": false
-    },
-    {
-        "Nome": "Andre Balduino Berte",
-        "codigo_de_rastreio": [
-            "AN441089975BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014518258190",
-        "cidade": "Porto Alegre",
-        "estado": "RS",
-        "done": false
-    },
-    {
-        "Nome": "Nathalie Do Nascimento Pires",
-        "codigo_de_rastreio": [
-            "AN441089927BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014509748100",
-        "cidade": "Osório",
-        "estado": "RS",
-        "done": false
-    },
-    {
-        "Nome": "Dorival Fernandes Dos Santos",
-        "codigo_de_rastreio": [
-            "AN441089944BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014502818116",
-        "cidade": "São Paulo",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Naif Abdo Faris",
-        "codigo_de_rastreio": [
-            "AN441089935BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014487580738",
-        "cidade": "Vilhena",
-        "estado": "RO",
-        "done": false
-    },
-    {
-        "Nome": "Luis Cezar Abib",
-        "codigo_de_rastreio": [
-            "AN441089958BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014474275152",
-        "cidade": "Mallet",
-        "estado": "PR",
-        "done": false
-    },
-    {
-        "Nome": "Veronice Fonseca Braga de Carvalho",
-        "codigo_de_rastreio": [
-            "AN447645985BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014593652080",
-        "cidade": "Montes Claros",
-        "estado": "MG",
-        "done": false
-    },
-    {
-        "Nome": "Alexandre Tavares Carvalho",
-        "codigo_de_rastreio": [
-            "AN447645971BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014586320588",
-        "cidade": "Brasília",
-        "estado": "DF",
+        "codigo_de_cada_marketplace": "2000014608409210",
+        "cidade": "Teresina",
+        "estado": "PI",
         "done": false
     }`.trim();
