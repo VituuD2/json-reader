26c3e880a112abed4a584b4d52318d9ddfeeaa3a
vituud2 <overroot0@gmail.com>
2026-01-13 15:19:15 -0300

    chore: update sample data with new entries

--- sample-data.js
+++ sample-data.js
@@ -1,27 +1,173 @@
 // Optional: you can delete this file if you don't want a sample.
 // app.js expects window.SAMPLE_DATA to exist (or it will just start empty).
 window.SAMPLE_DATA = `
-         {
-        "Nome": "Debora Bueno Muniz Oliveira",
-        "codigo_de_rastreio": [
-            "AN451936030BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014610404936",
-        "cidade": "São Paulo",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Danilo Prado Pires",
-        "codigo_de_rastreio": [
-            "AN451936043BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014608409210",
-        "cidade": "Teresina",
-        "estado": "PI",
-        "done": false
-    }`.trim();
+  {
+    "Nome": "Marilene Raphael",
+    "codigo_de_rastreio": ["AN475288873BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML2",
+    "codigo_de_cada_marketplace": "2000014696648390",
+    "cidade": "Palhoça",
+    "estado": "SC",
+    "done": false
+  },
+  {
+    "Nome": "Lucimara Barufi",
+    "codigo_de_rastreio": ["AN475288856BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML2",
+    "codigo_de_cada_marketplace": "2000014692664400",
+    "cidade": "Bady Bassitt",
+    "estado": "SP",
+    "done": false
+  },
+  {
+    "Nome": "Nilson Miranda",
+    "codigo_de_rastreio": ["AN469768972BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML2",
+    "codigo_de_cada_marketplace": "2000014677343386",
+    "cidade": "Pedro Leopoldo",
+    "estado": "MG",
+    "done": false
+  },
+  {
+    "Nome": "Gilberto Osorio Resende",
+    "codigo_de_rastreio": ["AN469768941BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML1",
+    "codigo_de_cada_marketplace": "2000014668659506",
+    "cidade": "Divinópolis",
+    "estado": "MG",
+    "done": false
+  },
+  {
+    "Nome": "Claudinei de Oliveira",
+    "codigo_de_rastreio": ["AN469768969BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML2",
+    "codigo_de_cada_marketplace": "2000014667590974",
+    "cidade": "Campinas",
+    "estado": "SP",
+    "done": false
+  },
+  {
+    "Nome": "Maria Vanderleia Aguiar",
+    "codigo_de_rastreio": ["AN469768924BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML3",
+    "codigo_de_cada_marketplace": "2000014666382344",
+    "cidade": "Santa Cruz das Palmeiras",
+    "estado": "SP",
+    "done": false
+  },
+  {
+    "Nome": "Gilvan Bueno de Morais Furtado Queiroz",
+    "codigo_de_rastreio": ["AN469768938BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML2",
+    "codigo_de_cada_marketplace": "2000014661280458",
+    "cidade": "Palmeiras de Goiás",
+    "estado": "GO",
+    "done": false
+  },
+  {
+    "Nome": "Gilvan Bueno de Morais Furtado Queiroz",
+    "codigo_de_rastreio": ["AN469769139BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML1",
+    "codigo_de_cada_marketplace": "2000014661271200",
+    "cidade": "Palmeiras de Goiás",
+    "estado": "GO",
+    "done": false
+  },
+  {
+    "Nome": "Tais Dal ben Casola",
+    "codigo_de_rastreio": ["AN469769187BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML3",
+    "codigo_de_cada_marketplace": "2000014655402056",
+    "cidade": "Lençóis Paulista",
+    "estado": "SP",
+    "done": false
+  },
+  {
+    "Nome": "DELICIO V",
+    "codigo_de_rastreio": ["AN469769173BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML3",
+    "codigo_de_cada_marketplace": "2000014644333178",
+    "cidade": "Barueri",
+    "estado": "SP",
+    "done": false
+  },
+  {
+    "Nome": "Patricia Menegazzi Caseiro",
+    "codigo_de_rastreio": ["AN469769156BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML2",
+    "codigo_de_cada_marketplace": "2000014639031724",
+    "cidade": "Campo Mourão",
+    "estado": "PR",
+    "done": false
+  },
+  {
+    "Nome": "Celso Marins Romanini",
+    "codigo_de_rastreio": ["AN469769142BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML1",
+    "codigo_de_cada_marketplace": "2000014635766298",
+    "cidade": "Curitiba",
+    "estado": "PR",
+    "done": false
+  },
+  {
+    "Nome": "Francisco Adolfo Valarini",
+    "codigo_de_rastreio": ["AN469769363BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML1",
+    "codigo_de_cada_marketplace": "2000014631246100",
+    "cidade": "Americana",
+    "estado": "SP",
+    "done": false
+  },
+  {
+    "Nome": "MARIANE H",
+    "codigo_de_rastreio": ["AN469769385BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML3",
+    "codigo_de_cada_marketplace": "2000014630771360",
+    "cidade": "Leme",
+    "estado": "SP",
+    "done": false
+  },
+  {
+    "Nome": "Jose Geraldo Guimaraes Alves",
+    "codigo_de_rastreio": ["AN469769350BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML2",
+    "codigo_de_cada_marketplace": "2000014619816994",
+    "cidade": "Americana",
+    "estado": "SP",
+    "done": false
+  },
+  {
+    "Nome": "Carlos Roberto Froes de Andrade",
+    "codigo_de_rastreio": ["AN469769346BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML1",
+    "codigo_de_cada_marketplace": "2000014617566582",
+    "cidade": "Barra Mansa",
+    "estado": "RJ",
+    "done": false
+  },
+  {
+    "Nome": "Paulo Cesar Baria de Castilho",
+    "codigo_de_rastreio": ["AN469769377BR"],
+    "telefone": null,
+    "codigo_de_marketplace": "ML2",
+    "codigo_de_cada_marketplace": "2000014617340228",
+    "cidade": "São José do Rio Preto",
+    "estado": "SP",
+    "done": false
+  }`.trim();
