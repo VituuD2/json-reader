6982923db6491116d89869f27d11e5290d137227
vituud2 <overroot0@gmail.com>
2026-01-23 15:59:10 -0300

    chore: sample size change

--- sample-data.js
+++ sample-data.js
@@ -2,266 +2,74 @@
 // app.js expects window.SAMPLE_DATA to exist (or it will just start empty).
 window.SAMPLE_DATA = `
   {
-        "Nome": "Nelmar Jose Moreira",
+        "Nome": "Daniel Rossi Lopes",
         "codigo_de_rastreio": [
-            "AN479359898BR"
+            "AN509446142BR"
         ],
         "telefone": null,
         "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014653568452",
-        "cidade": "Ponta Grossa",
-        "estado": "PR",
-        "done": false
-    },
-    {
-        "Nome": "Alex Renato Antonio",
-        "codigo_de_rastreio": [
-            "AN488227325BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014742039404",
-        "cidade": "São Carlos",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "ISABELLA GOULART",
-        "codigo_de_rastreio": [
-            "AN488227317BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014731878754",
-        "cidade": "Sapezal",
-        "estado": "MT",
-        "done": false
-    },
-    {
-        "Nome": "Zielle Bianca Bispo Alves",
-        "codigo_de_rastreio": [
-            "AN483518009BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014722103774",
-        "cidade": "Brasília",
-        "estado": "DF",
-        "done": false
-    },
-    {
-        "Nome": "Marco Antonio Alves Moro",
-        "codigo_de_rastreio": [
-            "AN483518012BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014717222576",
-        "cidade": "Campinas",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Ana Paula Mascaro",
-        "codigo_de_rastreio": [
-            "AN483518030BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014716483346",
-        "cidade": "Ribeirão Bonito",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Devair Ferreira da Cruz",
-        "codigo_de_rastreio": [
-            "AN483518026BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014712969068",
-        "cidade": "São José do Rio Preto",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "IDINEO FERREIRA DE ARAUJO",
-        "codigo_de_rastreio": [
-            "AN483526566BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014712764622",
-        "cidade": "São Simão",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Lucas Henrique Ferreira Dos Santos",
-        "codigo_de_rastreio": [
-            "AN496724494BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014778602612",
-        "cidade": "São José dos Campos",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Joao Maria Vaz Calvet de Magalhaes",
-        "codigo_de_rastreio": [
-            "AN496724517BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014777027580",
-        "cidade": "Santos",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Rafael Justus de Brito",
-        "codigo_de_rastreio": [
-            "AN496724477BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014773677008",
-        "cidade": "Curitiba",
-        "estado": "PR",
-        "done": false
-    },
-    {
-        "Nome": "Marcelo Joel Hoffmann",
-        "codigo_de_rastreio": [
-            "AN496724485BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014759761892",
-        "cidade": "Brasília",
-        "estado": "DF",
-        "done": false
-    },
-    {
-        "Nome": "Rogerio Santo Nichele",
-        "codigo_de_rastreio": [
-            "AN496724684BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014750939048",
-        "cidade": "Florianópolis",
-        "estado": "SC",
-        "done": false
-    },
-    {
-        "Nome": "Luiz Antonio Saboya Chiaradia",
-        "codigo_de_rastreio": [
-            "AN496724698BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014750958042",
-        "cidade": "São Paulo",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Kairo Correa Marques",
-        "codigo_de_rastreio": [
-            "AN496724503BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014748608716",
-        "cidade": "Pontes e Lacerda",
-        "estado": "MT",
-        "done": false    
-    },
-    {
-        "Nome": "Walbert Serrano Clerc",
-        "codigo_de_rastreio": [
-            "AN496829089BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014756342968",
-        "cidade": "São Paulo",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Márcia Vieira",
-        "codigo_de_rastreio": [
-            "AN496829101BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014766580444",
-        "cidade": "Florestal",
-        "estado": "MG",
-        "done": false
-    },
-    {
-        "Nome": "Silvia Aparecida Ferreira da Silva Donato",
-        "codigo_de_rastreio": [
-            "AN496829092BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML3",
-        "codigo_de_cada_marketplace": "2000014766262826",
-        "cidade": "Araras",
-        "estado": "SP",
-        "done": false
-    },
-    {
-        "Nome": "Gilberto Matos de Brito",
-        "codigo_de_rastreio": [
-            "AN496829075BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014757631472",
-        "cidade": "São Geraldo do Araguaia",
-        "estado": "PA",
-        "done": false
-    },
-    {
-        "Nome": "Edgar Cordeiro da Silva",
-        "codigo_de_rastreio": [
-            "AN496829129BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML2",
-        "codigo_de_cada_marketplace": "2000014753506512",
-        "cidade": "Matinhos",
-        "estado": "PR",
-        "done": false
-    },
-    {
-        "Nome": "Ivan Sergio Vaz Porto",
-        "codigo_de_rastreio": [
-            "AN496829340BR"
-        ],
-        "telefone": null,
-        "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014748132088",
-        "cidade": "Goiânia",
+        "codigo_de_cada_marketplace": "2000014813502364",
+        "cidade": "Anápolis",
         "estado": "GO",
         "done": false
     },
     {
-        "Nome": "Tatiana Tamburus Comachio",
+        "Nome": "Sergio Aparecido Braga de Azevedo",
         "codigo_de_rastreio": [
-            "AN496829319BR"
+            "AN509446156BR"
         ],
         "telefone": null,
         "codigo_de_marketplace": "ML1",
-        "codigo_de_cada_marketplace": "2000014748102068",
-        "cidade": "Ribeirão Preto",
+        "codigo_de_cada_marketplace": "2000014817005904",
+        "cidade": "Sete Lagoas",
+        "estado": "MG",
+        "done": false
+    },
+    {
+        "Nome": "Nicollas Servalo Miam",
+        "codigo_de_rastreio": [
+            "AN505925514BR"
+        ],
+        "telefone": null,
+        "codigo_de_marketplace": "ML1",
+        "codigo_de_cada_marketplace": "2000014805721074",
+        "cidade": "São Caetano do Sul",
+        "estado": "SP",
+        "done": false
+    },
+    {
+        "Nome": "Luiz Carlos Monteiro",
+        "codigo_de_rastreio": [
+            "AN505925505BR"
+        ],
+        "telefone": null,
+        "codigo_de_marketplace": "ML2",
+        "codigo_de_cada_marketplace": "2000014803782164",
+        "cidade": "Caxias do Sul",
+        "estado": "RS",
+        "done": false
+    },
+    {
+        "Nome": "Vinicius Valerio Kopp",
+        "codigo_de_rastreio": [
+            "AN501965019BR"
+        ],
+        "telefone": null,
+        "codigo_de_marketplace": "ML2",
+        "codigo_de_cada_marketplace": "2000014786859556",
+        "cidade": "Porto Alegre",
+        "estado": "RS",
+        "done": false
+    },
+    {
+        "Nome": "Marcela Zati Cocenza",
+        "codigo_de_rastreio": [
+            "AN501965022BR"
+        ],
+        "telefone": null,
+        "codigo_de_marketplace": "ML2",
+        "codigo_de_cada_marketplace": "2000014781442848",
+        "cidade": "São José do Rio Preto",
         "estado": "SP",
         "done": false
     }`.trim();
